f99daa1859ed1345f9baff870fe018cf0731b72a
Bill Dueber <bill@dueber.com>
2021-11-02 10:02:47 -0400

    Add ability to set an internal logger and use `increment_and_report`

--- lib/waypoint.rb
+++ lib/waypoint.rb
@@ -6,17 +6,17 @@ require_relative "ppnum"
 # processes for which you want to kick out log files with ongoing progress.
 class Waypoint
 
-  attr_accessor :batch_size_target, :file_or_process
+  attr_accessor :batch_size_target, :name, :logger
   attr_reader :last_batch_divsor, :last_batch_seconds, :last_batch_size,
               :start_time, :batch_start_time, :batch_end_time,
               :count, :prev_count
 
-  def initialize(batch_size: 100, file_or_process: nil)
+  def initialize(batch_size: 100, name: nil, logger: nil)
     @batch_size_target  = batch_size
     @last_batch_divsor  = 0
     @last_batch_size    = 0
     @last_batch_seconds = 0
-    @file_or_process    = file_or_process
+    @name = name
 
     @start_time       = Time.now
     @batch_start_time = @start_time
@@ -73,9 +73,7 @@ class Waypoint
   # @return [String] Rate-per-second in form XXX.YY
   def batch_rate_str(decimals = 0)
     return "0" if @count.zero?
-
-    ppnum(last_batch_size.to_f / last_batch_seconds)
-    # format "%5.#{decimals}f",
+    ppnum(last_batch_size.to_f / last_batch_seconds, 0, decimals)
   end
 
   # @param [Integer] decimals Number of decimal places to the right of the
@@ -83,26 +81,73 @@ class Waypoint
   # @return [String] Rate-per-second in form XXX.YY
   def total_rate_str(decimals = 0)
     return "0" if @count.zero?
-
-    ppnum(count / total_seconds_so_far)
+    ppnum(count / total_seconds_so_far, 0, decimals)
   end
 
   def batch_line
     # rubocop:disable Layout/LineLength
-    "#{file_or_process} #{ppnum(count, 10)}. This batch #{ppnum(last_batch_size, 5)} in #{ppnum(last_batch_seconds, 4, 1)}s (#{batch_rate_str} r/s). Overall #{total_rate_str} r/s."
+    "#{name} #{ppnum(count, 10)}. This batch #{ppnum(last_batch_size, 5)} in #{ppnum(last_batch_seconds, 4, 1)}s (#{batch_rate_str} r/s). Overall #{total_rate_str} r/s."
     # rubocop:enable Layout/LineLength
   end
 
   def final_line
     # rubocop:disable Layout/LineLength
-    "#{file_or_process} FINISHED. #{ppnum(count, 10)} total records in #{seconds_to_time_string(total_seconds_so_far)}. Overall #{total_rate_str} r/s."
+    "#{name} FINISHED. #{ppnum(count, 10)} total records in #{seconds_to_time_string(total_seconds_so_far)}. Overall #{total_rate_str} r/s."
     # rubocop:enable Layout/LineLength
+  end
+
+  def log(msg, level: :info)
+    if logger
+      logger.send(level, msg)
+    end
+  end
+
+  def log_batch_line(level: :info)
+      on_batch { log(batch_line, level: level) }
+  end
+
+  def log_final_line(level: info)
+    log(final_line, level: level)
+  end
+
+  def increment_and_report(level: :info)
+    self.incr
+    log_batch_line(level)
   end
 
   def seconds_to_time_string(sec)
     hours, leftover = sec.divmod(3600)
     minutes, secs   = leftover.divmod(60)
     format("%02dh %02dm %02ds", hours, minutes, secs)
+  end
+
+  class Structured < Waypoint
+
+    def batch_line
+      {
+        name: name,
+        batch_count: last_batch_size,
+        batch_seconds: last_batch_seconds,
+        batch_rate: count.zero? ? 0 : last_batch_size.to_f / last_batch_seconds,
+        total_count: count,
+        total_seconds: total_seconds_so_far,
+        total_rate: count.zero? ? 0 : count.to_f / total_seconds_so_far
+      }
+    end
+
+    alias_method :batch_data, :batch_line
+
+    def final_line
+      {
+        name: name,
+        total_count: count,
+        total_seconds: total_seconds_so_far,
+        total_rate: count.zero? ? 0 : count.to_f / total_seconds_so_far
+      }
+    end
+
+    alias_method :final_data, :final_line
+
   end
 
 end
--- lib/waypoint/version.rb
+++ lib/waypoint/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Waypoint
-  VERSION = "0.1.2"
+  VERSION = "0.2"
 end
